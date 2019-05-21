c0c669d4e18c585ca9eaa8ad70376157d852c872
paulogonzalez5679 <paulomga10@gmail.com>
2019-05-21 02:41:09 -0500

    se ejecuta las imagenes randomicas

--- Galeria/pasar.js
+++ Galeria/pasar.js
@@ -1,4 +1,34 @@
 
+function rand(n)
+{
+    return(Math.floor(Math.random()*10+1))
+}
+
+var cambiar= new Array();
+cambiar[0]= "foto1.jpg";
+cambiar[1]= "foto2.jpg";
+cambiar[2]= "foto3.jpg";
+cambiar[3]= "foto4.jpg";
+cambiar[4]= "foto5.jpg";
+cambiar[5]= "foto6.jpg";
+cambiar[6]= "foto7.jpg";
+cambiar[7]= "foto8.jpg";
+cambiar[8]= "foto9.jpg";
+cambiar[9]= "foto10.jpg";
+
+
+
+
+function random()
+{
+    var foto=document.getElementById("foto").src = cambiar[rand(10)-1];
+    foto.src="foto"+num+".jpg"
+}
+
+
+
+
+
 var num=1;
 function siguiente()
 {
