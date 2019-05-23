c3743b1a55d77b4f303fae07f4d1c6ce9442d323
paulogonzalez5679 <paulomga10@gmail.com>
2019-05-23 03:59:12 -0500

    mejora validaciones

--- Formulario de validacion/valida.js
+++ Formulario de validacion/valida.js
@@ -104,84 +104,28 @@ function sololetras(elemento) {
 
 function dosPalabras(e) { 
     tecla = (document.all) ? e.keyCode : e.which; 
-    if (tecla==8) return true; //Tecla de retroceso (para poder borrar) 
-    // dejar la línea de patron que se necesite y borrar el resto 
+    if (tecla==8) return true; 
     patron =/^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\']+[\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])$/; // Solo acepta letras 
-    //patron = /\d/; // Solo acepta números 
-    //patron = /\w/; // Acepta números y letras 
-    //patron = /\D/; // No acepta números 
-    // 
     te = String.fromCharCode(tecla); 
     return patron.test(te); 
     } 
 
 
-/*
-    
-    
-    //key=nombre.keyCode || nombre.which;
-    var m = document.getElementById("nombre").value;
-    teclado=String.fromCharCode(key).toLowerCase();
-    var expreg = /^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\']+[\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])$/;
-  
-    if(expreg.test(m))
-      alert("La matrícula es correcta");
-    else
-      alert("Solo puede ingresar dos palabras");
-}
-/*
-function dosPalabras(elemento){
-    
-    key=elemento.keyCode || elemento.which;
-    teclado=String.fromCharCode(key).toLowerCase();
-    letras="qwertyuiopasdfghjklñzxcvbnm ";
-    especiales="8-37-38-46-164";
-    teclado_especial=false;
-    
- 
-    // Patron palabra espacio palabra
-    
-    var patron= /^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\']+[\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])$/;
-	if(valueForm.search(patron)==0)
-	{
-		//Mail correcto
-		
-		object.style.color="#000";
-		return;
-	}
-	//Mail incorrecto
-	
-    object.style.color="#f00";
-    
-}
-*/
+
 
 function ValidarFecha(fechaNac){
     object=document.getElementById(fechaNac);
-    // Almacenamos el valor digitado en TxtFecha
     var Fecha = document.getElementById('fechaNac').value;
     var Mensaje = '';
     document.getElementById('Mensaje').className = '';
     
-    // Si la fecha está completa comenzamos la validación
     if(Fecha.length == 8){
 
-        var Anio = Fecha.substr(4, 4); // Extraemos en año
-        var Mes = parseFloat(Fecha.substr(2, 2)) - 1; // Extraemos el mes
-        var Dia = Fecha.substr(0, 2); // Extraemos el día
-    
-     //var Anio = Fecha.substr(0, 4); // Extraemos en año
-     //var Mes = parseFloat(Fecha.substr(4, 2)) - 1; // Extraemos el mes
-     //var Dia = Fecha.substr(6, 2); // Extraemos el día
-     
-     // Con la función Date() de javascript evaluamos si la fecha existe
+        var Anio = Fecha.substr(4, 4); 
+        var Mes = parseFloat(Fecha.substr(2, 2)) - 1; 
+        var Dia = Fecha.substr(0, 2); 
      var VFecha = new Date(Anio, Mes, Dia);
-     
-     // Si las partes de la fecha concuerdan con las que digitamos, es correcta
      if((VFecha.getFullYear() == Anio) && (VFecha.getMonth() == Mes) && (VFecha.getDate() == Dia)){
-        //Mensaje = 'Fecha correcta';
-        //document.getElementById('Mensaje').className = 'Mensaje1';
-
         object.style.color="#000";
         return; 
      }
@@ -189,34 +133,16 @@ function ValidarFecha(fechaNac){
 
         object.style.color="#f00";
 
-     //Mensaje = 'Fecha incorrecta';
-     //document.getElementById('Mensaje').className = 'Mensaje';
+
      }
 
-     /*
-    //if(Anio > 2019){
-        Mensaje = 'Año incorrecto';
-        document.getElementById('Mensaje').className = 'Mensaje';
-     }
-    // if(Mes > 12){
-        Mensaje = 'Mes incorrecto';
-        document.getElementById('Mensaje').className = 'Mensaje';
-     }
-    // if(Dia >31){
-        Mensaje = 'Dia incorrecto';
-        document.getElementById('Mensaje').className = 'Mensaje';
-     }
-     */
-    }
     
-    // Mostramos el mesaje
+    
     document.getElementById('Mensaje').innerHTML = Mensaje;
 }
 
 
 
-
-//VALIDACION SOLO NUMEROS  
 
 
 function soloNumeros(elemento){
@@ -244,31 +170,24 @@ function soloNumeros(elemento){
 
 
 
-//Correo Institucional UPS
+
 
 function ValidarMail(email){
-	//Creamos un objeto 
 	object=document.getElementById(email);
 	valueForm=object.value;
  
-	// Patron para el correo
     var patron=/^\w+([\.-]{3,8}?\w+)*@(?:est.ups.edu.ec|ups.edu.ec|)$/;
-    // /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
 	if(valueForm.search(patron)==0)
 	{
-		//Mail correcto
+		
 		
 		object.style.color="#000";
 		return;
 	}
-	//Mail incorrecto
 	
     object.style.color="#f00";
     
 }
-
-
-//FUNCION VALIDAR CEDULA ECUADOR 
 
     
 function cedulaEC(cedula) {
@@ -285,14 +204,13 @@ function cedulaEC(cedula) {
           if (aux > 9) aux -= 9;
           total += aux;
         } else {
-          total += parseInt(cad.charAt(i)); // parseInt o concatenará en lugar de sumar
+          total += parseInt(cad.charAt(i)); 
         }
       }
 
       total = total % 10 ? 10 - total % 10 : 0;
 
       if (cad.charAt(longitud-1) == total) {
-       // document.getElementById("salida").innerHTML = ("Cedula Válida");
        object.style.color="#000"; 
        return;
 
@@ -302,4 +220,4 @@ function cedulaEC(cedula) {
       }
     }
   }
-
+}
